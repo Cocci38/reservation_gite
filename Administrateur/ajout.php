19fbd555cf9a7865a7582e853418633ae49305b0
ibtissemdev <ibtissem.khiri@gmail.com>
2022-01-13 10:35:50 +0100

    categorie

--- Administrateur/ajout.php
+++ Administrateur/ajout.php
@@ -3,26 +3,24 @@
 require 'initialisation.php';
 
 if (empty($_SESSION['result'])) {
-    header("location:connexion.php");
+    header("location:connexion.php");}
 
 
 $intitule = $_POST["Intitule"];
-$categorie = $_POST["Categorie"];
 $description = $_POST["Description"];
 $photo = $_POST["Photo"];
 $couchage = $_POST["Nombre_de_couchages"];
 $bain = $_POST["Nombre_de_salles_de_bain"];
 $lieux = $_POST["Emplacement_geographique"];
 $prix = $_POST["Prix"];
-}
+
 
 try{
 $sth = $conn->prepare("
-    INSERT INTO hebergements(Intitule, Categorie, Description, Photo, Nombre_de_couchages, Nombre_de_salles_de_bain, Emplacement_geographique, Prix)
-    VALUES(:Intitule, :Categorie, :Description, :Photo, :Nombre_de_couchages, :Nombre_de_salles_de_bain, :Emplacement_geographique, :Prix)");
+    INSERT INTO hebergements(Intitule, Description, Photo, Nombre_de_couchages, Nombre_de_salles_de_bain, Emplacement_geographique, Prix)
+    VALUES(:Intitule, :Description, :Photo, :Nombre_de_couchages, :Nombre_de_salles_de_bain, :Emplacement_geographique, :Prix)");
 
     $sth->bindParam(':Intitule',$intitule);
-    $sth->bindParam(':Categorie',$categorie);
     $sth->bindParam(':Description',$description);
     $sth->bindParam(':Photo',$photo);
     $sth->bindParam(':Nombre_de_couchages',$couchage);
@@ -31,12 +29,19 @@ $sth = $conn->prepare("
     $sth->bindParam(':Prix',$prix);
     $sth->execute();
 
+
+
+
     //header();
     
 }
 catch (PDOException $e) {
     echo 'Impossible de traiter les données. Erreur : ' . $e->getMessage();
 }
+
+
+
+
 
 
 
---
+++ Administrateur/categorie.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+<form action= '' method="post">
+
+        <label for="Nom">Nom de la catégorie</label>
+        <input type="text" name="Nom" id="Nom" required>
+        <input type="submit" value="Envoyer">
+
+        </form>
+
+
+
+
+<?php
+
+require 'initialisation.php';
+
+if (empty($_SESSION['result'])) {
+    header("location:connexion.php");}
+
+       
+
+        try 
+        {
+            $nom = $_POST["Nom"];
+
+            $sth = $conn->prepare("
+            INSERT INTO categories (Nom)
+            VALUES (:Nom)");
+
+            $sth->bindParam(':Nom',$nom);
+            $sth->execute();
+
+        }
+
+    
+    catch (PDOException $e) {
+        echo 'Impossible de traiter les données. Erreur : ' . $e->getMessage();
+    }
+    
+?>
+
+
+</body>
+</html>
