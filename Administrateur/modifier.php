493c42ce8bf99782d061e4cd0839cbdea834fbb9
ibtissemdev <ibtissem.khiri@gmail.com>
2022-01-14 15:53:10 +0100

    categorie

--- Administrateur/modifier.php
+++ Administrateur/modifier.php
@@ -6,7 +6,6 @@ if (empty($_SESSION['result'])) {
     header("location:connexion.php");
 
 $intitule = $_POST["Intitule"];
-$categorie = $_POST["Categorie"];
 $description = $_POST["Description"];
 $photo = $_POST["Photo"];
 $couchage = $_POST["Nombre_de_couchages"];
@@ -16,11 +15,10 @@ $prix = $_POST["Prix"];
 }
 
 try{
-    $sth = $conn->prepare(" UPDATE hebergements SET Intitule= :Intitule, Categorie= :Categorie, Description = :Description, Photo= :Photo, Nombre_de_couchages= :Nombre_de_couchages, Nombre_de_salles_de_bain= :Nombre_de_salles_de_bain, Emplacement_geographique= :Emplacement_geographique, Prix= :Prix where Id= :id"); 
+    $sth = $conn->prepare(" UPDATE hebergements SET Intitule= :Intitule, Description= :Description, Photo= :Photo, Nombre_de_couchages= :Nombre_de_couchages, Nombre_de_salles_de_bain= :Nombre_de_salles_de_bain, Emplacement_geographique= :Emplacement_geographique, Prix= :Prix where Id= :id"); 
  
     
         $sth->bindParam(':Intitule',$intitule);
-        $sth->bindParam(':Categorie',$categorie);
         $sth->bindParam(':Description',$description);
         $sth->bindParam(':Photo',$photo);
         $sth->bindParam(':Nombre_de_couchages',$couchage);
