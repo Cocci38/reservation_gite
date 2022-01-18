8f96d37a4eb165ab85e93f912311510e685aa2b1
ibtissemdev <ibtissem.khiri@gmail.com>
2022-01-18 15:21:29 +0100

    photo.3

--- Administrateur/indexheb.php
+++ Administrateur/indexheb.php
@@ -1,19 +1,41 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="fr">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- <link rel="stylesheet" href="../CSS\style.global.css"> -->
+    <link rel="stylesheet" href="../CSS\style.admin.css">
+
+
     <title>Document</title>
 </head>
 <body>
-
 <button><a href="deconnexion.php">Deconnexion</a></button> 
+
+
+<button><a href="../utilisateur\listegite.php">Utilisateur</a></button> 
+<header>
+
+
+
+<nav class="container--fluid">
+<div class="item">
+    <a href="indexheb.php"><img class = "logo" src="../imageeasytrip\Logo_easy_trip.png" alt="logo_easy_trip" srcset=""></a>
+</div>
+<div class="item">
+    <h1>Avec nous voyager facile !</h1>
+</div>
+</nav>
+</header>
 
 <button><a href="ajout_hebergement.php">Ajouter un hébergement</a></button> 
 
-<?php
+<h2>Liste des hébergements</h2>
 
+
+
+<?php
 
 
 
@@ -37,13 +59,21 @@ try {
     $result = $sth->fetchAll();
 
     /*echo  '<pre>'; var_dump($result); echo '</pre>';*/
+?>
 
+<div class="block">
+
+<?php 
     for ($y = 0; $y < count($result); $y++) {
-
-        echo '<a href="./afficher.hebergement.php?id=' . $result[$y]['Id'] . '">' .  $result[$y]['Intitule']. '</a><br>' . '<br>';
+       
+        echo '<a href="./afficher.hebergement.php?id=' . $result[$y]['Id'] . '">' .  $result[$y]['Intitule'].  '</a><br>' . '<br>';
+        echo  '<img src= "../images/'. $result [$y] ['Photo']. '" alt="">';
     }
 
- 
+    ?>
+
+    </div>
+ <?php 
 
 }
 
@@ -52,7 +82,9 @@ try {
             }
 
 ?>
-  
+</main>
+
+</div>
 </body>
 </html>
 
