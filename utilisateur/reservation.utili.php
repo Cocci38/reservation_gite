b4bfac783cdb79e23187b83b1772c5038f18e418
Sandra <sandra.villot038@gmail.com>
2022-01-20 18:28:07 +0100

    css reservation

--- utilisateur/reservation.utili.php
+++ utilisateur/reservation.utili.php
@@ -20,65 +20,68 @@
     <main>
 
     <h2>Réservation</h2>
-            <form class="reservation" action="reservation.php" method="post">
+            <form class="reservation-container" action="reservation.php" method="post">
             <div class = "date">
                     <h3>Vos dates de séjour</h3>
-                <div>
+                <div class="arrive-depart">
+                <div class="arrive">
                     <label for="arrivee">Du</label>
                     <input type="date" id="arrivee" name="arrivee" required>
                 </div>
-                <div>
+                <div class="depart">
                     <label for="depart">Au</label>
                     <input type="date" id="depart" name="depart" required>
                 </div>
                 </div>
+            </div>
                 <div class = "personne">
-                    <h3>Nombres de personnes</h3>
-                <div>
+                    <h3 class="nb-personne">Nombres de personnes</h3>
+                    <div class="nombre-personne">
+                <div class="adulte">
                     <label for="adulte">Adulte</label>
                     <input type="text" id="adulte" name="adulte" required>
                 </div>
-                <div>
+                <div class="enfant">
                     <label for="enfant">Enfant</label>
                     <input type="text" id="enfant" name="enfant" required>
                 </div>
                 </div>
+                </div>
                 <div class = "corps">
-                <div>
+                    <h3 class="vos-coordonnees">Vos coordonnées</h3>
+                <div class="coordonne">
+                    <div class="item1">
                     <label for="titre">Titre</label>
                     <input type="text" id="titre" name="titre" required>
-                </div>
-                <div>
+                    </div>
+                    <div class="item2">
                     <label for="name">Nom</label>
                     <input type="text" id="name" name="nom" required>
-                </div>
-                <div>
+                    </div>
+                    <div class="item3">
                     <label for="prenom">Prénom</label>
                     <input type="text" id="prenom" name="prenom" required>
-                </div>
-                <div>
+                    </div>
+                    <div class="item4">
                     <label for="adresse">Adresse</label>
                     <input type="text" id="adresse" name="adresse"required>
-                </div>
-                <div>
+                    </div>
+                    <div class="item5">
                     <label for="code_postal">Code Postal</label>
                     <input type="text" id="code_postal" name="code_postal" required>
-                </div>
-                <div>
+                    </div>
+                    <div class="item6">
                     <label for="ville">Ville</label>
                     <input type="text" id="ville" name="ville" required>
-                </div>
-                <div>
-                    <label for="pays">Pays</label>
-                    <input type="text" id="pays" name="pays" required>
-                </div>
-                <div>
+                    </div>
+                    <div class="item7">
                     <label for="phone">Tel</label>
                     <input type="tel" id="phone" name="telephone" required>
-                </div>
-                <div>
+                    </div>
+                    <div class="item8">
                     <label for="mail">e-mail</label>
                     <input type="email" id="mail" name="mail" required>
+                    </div>
                 </div>
                 </div>
                 <div class="btn">
