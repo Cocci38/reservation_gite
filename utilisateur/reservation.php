4125a23fd9d8951a0be3c5dd72ba6c8b6258b30c
ibtissemdev <ibtissem.khiri@gmail.com>
2022-01-25 12:05:52 +0100

    fonction mail

--- utilisateur/reservation.php
+++ utilisateur/reservation.php
@@ -3,7 +3,7 @@
 require '../Administrateur\initialisation.php';
 
 $arrivee = $_POST["arrivee"];
-$Id_hebergement = $_POST["Id_hebergement"];
+/*$Id_hebergement = $_POST["Id_hebergement"];*/
 $depart = $_POST["depart"];
 $adulte = $_POST["adulte"];
 $enfant = $_POST["enfant"];
@@ -19,11 +19,11 @@ $mail = $_POST["mail"];
 
 
 try{
-$sth = $conn->prepare("INSERT INTO Reservation_clients(arrivee, Id_hebergement, depart, adulte, enfant, titre, nom, prenom, adresse, code_postal, ville, pays, telephone, mail)
-                    VALUES(:arrivee, :Id_hebergement, :depart, :adulte, :enfant, :titre, :nom, :prenom, :adresse, :code_postal, :ville, :pays, :telephone, :mail)");
+$sth = $conn->prepare("INSERT INTO Reservation_clients(arrivee, depart, adulte, enfant, titre, nom, prenom, adresse, code_postal, ville, pays, telephone, mail)
+                    VALUES(:arrivee, :depart, :adulte, :enfant, :titre, :nom, :prenom, :adresse, :code_postal, :ville, :pays, :telephone, :mail)");
 
     $sth->bindParam(':arrivee',$arrivee);
-    $sth->bindParam(':Id_hebergement',$Id_hebergement);
+    /*$sth->bindParam(':Id_hebergement',$Id_hebergement);*/
     $sth->bindParam(':depart',$depart);
     $sth->bindParam(':adulte',$adulte);
     $sth->bindParam(':enfant',$enfant);
@@ -38,8 +38,40 @@ $sth = $conn->prepare("INSERT INTO Reservation_clients(arrivee, Id_hebergement, 
     $sth->bindParam(':mail',$mail);
     $sth->execute();
 
-    header('Location: msg.mail.php');
-    
+    /*header('Location: msg.mail.php');*/
+
+    if (isset ($_POST['envoyer']) && !empty($_POST['mail'])) {
+
+        echo "ok";
+        var_dump($_POST['mail']);
+
+        $mail = $_POST['mail'];
+  
+        $to = $mail;
+        $subject = "Confirmation réservation";
+        $message = "Nous vous remercions pour votre réservation. <br> Nous espéront que vous apprecierez votre séjour. <br> l'équipe Easy Trip ";
+        
+        // Pour revenir à la ligne tous les 70 caractères environ
+        
+        $message = wordwrap($message, 70, "\r\n");
+        
+        // Mettre un destinataire : 
+        
+        $headers= "From: ibtissem.khir@gmail.com";
+        
+        
+        mail($to,$subject,$message,$headers);
+
+
+
+
+       }
+
+
+
+
+
+
 }
 catch (PDOException $e) {
     echo 'Impossible de traiter les données. Erreur : ' . $e->getMessage();
