3e8c3e26c373a54697a9a1034822ee70276ac53a
Sandra <sandra.villot038@gmail.com>
2022-04-25 16:58:25 +0200

    amélioration disponibilité et css

---
+++ CSS/afficher.hebergement.php
@@ -0,0 +1,89 @@
+<!DOCTYPE html>
+<html lang="fr,en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../CSS\style.admin.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@100;500&display=swap" rel="stylesheet">
+    <link rel="icon" type="images/png" href="images-memory\black.png">
+    <title>Hébergements</title>
+</head>
+
+<body>
+
+<header>
+
+<nav class="container--fluid">
+<div class="item">
+    <a href="indexheb.php"><img class = "logo" src="../imageeasytrip\Logo_easy_trip.png" alt="logo_easy_trip" srcset=""></a>
+</div>
+<div class="item">
+    <h1>Avec nous voyager facile !</h1>
+</div>
+</nav>
+</header>      
+    
+<button><a href="deconnexion.php">Deconnexion</a></button> 
+<button><a href="indexheb.php">Accueil</a></button> 
+<button><a href="../utilisateur\accueil.php">Utilisateur</a></button> 
+<main>
+
+<?php
+
+// Afficher la fiche hébergement
+
+try {
+
+require 'initialisation.php';
+
+if (empty($_SESSION['result'])) {
+    header("location:connexion.php");
+    // echo "Identifiant ou mot de passe incorrect";
+}
+/* Jointure*/
+$sth = $conn->prepare("SELECT * FROM hebergements LEFT JOIN categories ON hebergements.Id_categorie = categories.Id where hebergements.Id= :id");
+
+        $sth -> bindValue (":id" , $_GET['id'] );
+        $sth->execute();
+        $result = $sth->fetch();
+
+    if (isset($result)) {
+?>
+<div class="fiche_hebergement">
+    <span><?php  echo $result ['Intitule'] ;?></span>
+    <span><?php  echo " Catégorie : " . $result ['Nom'] ;?></span>
+    <span><?php echo " Description : " . $result ['Description'] ;?></span>
+    <span><?php echo " Nbr de couchages : " . $result ['Nombre_de_couchages'] ;?></span>
+    <span><?php echo " Nbr de salles de bains : " . $result ['Nombre_de_salles_de_bain'] ;?></span>
+    <span><?php echo " Emplacement : " . $result ['Emplacement_geographique'] ;?></span>
+    <span><?php echo " Prix : " . $result ['Prix'] ;?></span>
+    <span><?php echo " Photo 1 : "  . '<img src= "../images/'. $result ['Nom1']. '" alt="photo hébergement">';?></span>
+    <span><?php echo " Photo 2 : "  . '<img src= "../images/'. $result ['Nom2']. '" alt="photo hébergement">';?></span>
+    <span><?php echo " Photo 3 : "  . '<img src= "../images/'. $result ['Nom3']. '" alt="photo hébergement">';?></span>
+    <span><?php echo " Photo 4 : "  . '<img src= "../images/'. $result ['Nom4']. '" alt="photo hébergement">';?></span>
+    <span><?php echo " Photo 5 : "  . '<img src= "../images/'. $result ['Nom5']. '" alt="photo hébergement">';?></span>
+
+    <div class="btn-crud">
+        <button> <?php echo'<a href="modifier.hebergement.php?id='.$_GET['id'].'"">Modifier hébergement</a>'; ?></button>
+
+        <button><?php echo'<a href="supprimer.php?id='. $_GET['id'] .'">Supprimer hébergement</a>';?></button>
+    </div>             
+</div>    
+<?php
+
+}
+} catch (PDOException $e) {
+    echo 'Impossible de traiter les données. Erreur : ' . $e->getMessage();
+}
+?>
+            </div>
+        </div>
+    </div>
+    </main>
+    <?php require 'footer.php';?>
+
+</body></html>
--- utilisateur/reservation.utili.php
+++ utilisateur/reservation.utili.php
@@ -22,100 +22,109 @@
 try{
     require '../Administrateur\initialisation.php';
 
-$sth2 = $conn->prepare("SELECT Id, Intitule, Emplacement_geographique, Disponibilite FROM hebergements where Id= :id");
+$sth2 = $conn->prepare("SELECT * FROM hebergements where Id= :id");
 $sth2 -> bindValue (":id" , $_GET['Id'] );
 $sth2->execute();
 $resultat = $sth2->fetch(PDO::FETCH_ASSOC);
-
-
 ?>
-<h2><?php echo $resultat ['Intitule'] ; ?></h2>
-    <?php echo  " en " . $resultat ['Emplacement_geographique'] ; ?></p>
 
+<h2 class="reservation">Réservation</h2>
 
-    <h2>Réservation</h2>
-            <form class="reservation-container" action="reservation.php" method="post">
-            <input type="hidden" name="Id" value="<?php echo $resultat['Id']; ?>">
-            <input type="hidden" name="Disponibilite" value="<?php echo $resultat['Disponibilite']; ?>">
+<div class="reservation-container">
 
-            <div class = "date">
-                    <h3>Vos dates de séjour</h3>
-                <div class="arrive-depart">
-                <div class="arrive">
-                    <label for="arrivee">Du</label>
-                    <input type="date" id="arrivee" name="arrivee" required>
+<div class="hebergement">
+    <h3>Votre hébergement :</h3>
+    <h4><?= $resultat ['Intitule'] ; ?></h4>
+    <?= '<img src= "../images/'. $resultat ['Nom1']. '" alt="photo hébergement" width = "300px">'; ?>
+    <h4>Description :</h4>
+    <?=  $resultat ['Description'] ; ?>
+    <?= '<a href="./fiche.hebergement.php?Id='. $resultat['Id'] . '"> Consulter la fiche de l\'hébergement </a><br>' . '<br>';?>
+    </div>
+</br>
+
+    <form action="reservation.php" method="post">
+        <input type="hidden" name="Id" value="<?php echo $resultat['Id']; ?>">
+        <input type="hidden" name="Disponibilite" value="<?php echo $resultat['Disponibilite']; ?>">
+
+        <div class = "date">
+                <h3>Vos dates de séjour</h3>
+            <div class="arrive-depart">
+            <div class="arrive">
+                <label for="arrivee">Du</label>
+                <input type="date" id="arrivee" name="arrivee" required>
+            </div>
+            <div class="depart">
+                <label for="depart">Au</label>
+                <input type="date" id="depart" name="depart" required>
+            </div>
+            </div>
+        </div>
+            <div class = "personne">
+                <h3 class="nb-personne">Nombres de personnes</h3>
+                <div class="nombre-personne">
+            <div class="adulte">
+                <label for="adulte">Adulte</label>
+                <input type="text" id="adulte" name="adulte" required>
+            </div>
+            <div class="enfant">
+                <label for="enfant">Enfant</label>
+                <input type="text" id="enfant" name="enfant" required>
+            </div>
+            </div>
+            </div>
+            <div class = "corps">
+                <h3 class="vos-coordonnees">Vos coordonnées</h3>
+            <div class="coordonne">
+                <div class="item1">
+                <label for="titre">Titre</label>
+                <input type="text" id="titre" name="titre" required>
                 </div>
-                <div class="depart">
-                    <label for="depart">Au</label>
-                    <input type="date" id="depart" name="depart" required>
+                <div class="item2">
+                <label for="name">Nom</label>
+                <input type="text" id="name" name="nom" required>
                 </div>
+                <div class="item3">
+                <label for="prenom">Prénom</label>
+                <input type="text" id="prenom" name="prenom" required>
+                </div>
+                <div class="item4">
+                <label for="adresse">Adresse</label>
+                <input type="text" id="adresse" name="adresse"required>
+                </div>
+                <div class="item5">
+                <label for="code_postal">Code Postal</label>
+                <input type="text" id="code_postal" name="code_postal" required>
+                </div>
+                <div class="item6">
+                <label for="ville">Ville</label>
+                <input type="text" id="ville" name="ville" required>
+                </div>
+                <div class="item7">
+                <label for="pays">Pays</label>
+                <input type="text" id="pays" name="pays" required>
+                </div>
+                <div class="item8">
+                <label for="phone">Téléphone</label>
+                <input type="tel" id="phone" name="telephone" required>
+                </div>
+                <div class="item9">
+                <label for="mail">e-mail</label>
+                <input type="email" id="mail" name="mail" required>
                 </div>
             </div>
-                <div class = "personne">
-                    <h3 class="nb-personne">Nombres de personnes</h3>
-                    <div class="nombre-personne">
-                <div class="adulte">
-                    <label for="adulte">Adulte</label>
-                    <input type="text" id="adulte" name="adulte" required>
-                </div>
-                <div class="enfant">
-                    <label for="enfant">Enfant</label>
-                    <input type="text" id="enfant" name="enfant" required>
-                </div>
-                </div>
-                </div>
-                <div class = "corps">
-                    <h3 class="vos-coordonnees">Vos coordonnées</h3>
-                <div class="coordonne">
-                    <div class="item1">
-                    <label for="titre">Titre</label>
-                    <input type="text" id="titre" name="titre" required>
-                    </div>
-                    <div class="item2">
-                    <label for="name">Nom</label>
-                    <input type="text" id="name" name="nom" required>
-                    </div>
-                    <div class="item3">
-                    <label for="prenom">Prénom</label>
-                    <input type="text" id="prenom" name="prenom" required>
-                    </div>
-                    <div class="item4">
-                    <label for="adresse">Adresse</label>
-                    <input type="text" id="adresse" name="adresse"required>
-                    </div>
-                    <div class="item5">
-                    <label for="code_postal">Code Postal</label>
-                    <input type="text" id="code_postal" name="code_postal" required>
-                    </div>
-                    <div class="item6">
-                    <label for="ville">Ville</label>
-                    <input type="text" id="ville" name="ville" required>
-                    </div>
-                    <div class="item7">
-                    <label for="pays">Pays</label>
-                    <input type="text" id="pays" name="pays" required>
-                    </div>
-                    <div class="item8">
-                    <label for="phone">Téléphone</label>
-                    <input type="tel" id="phone" name="telephone" required>
-                    </div>
-                    <div class="item9">
-                    <label for="mail">e-mail</label>
-                    <input type="email" id="mail" name="mail" required>
-                    </div>
-                </div>
-                </div>
-                <div class="btn">
-                <div>
-               
-                    <button class="envoyer" type="submit" name="envoyer"  > Réserver </button>
-                </div>
-                <div>
-                <button class="retour2" type = "button" value = "Retour"  onclick = "history.go(-1)">Retour</button>
-                </div>
-                </div>
-            </form>
-            <?php
+            </div>
+            <div class="btn">
+            <div>
+            
+                <button class="envoyer" type="submit" name="envoyer"  > Réserver </button>
+            </div>
+            <div>
+            <button class="retour2" type = "button" value = "Retour"  onclick = "history.go(-1)">Retour</button>
+            </div>
+            </div>
+        </form>
+        </div>
+        <?php
             
 }
 catch(PDOException $e){
